cf94bf64196b6f1e998363d361e33c5eaff6451b
El Pepon <elpepon@iMac.local>
2019-10-13 12:03:48 -0300

    ..

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://cafe:49xtdrown@cluster0-lwu8t.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 };
 process.env.URLDB = urlDB;
