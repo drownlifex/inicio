ec2e9cc7a17db7bce92e5d7730d1fc760591bbdd
El Pepon <elpepon@iMac.local>
2019-10-13 12:07:34 -0300

    dfkjsdfh

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = process.env.MONGO_URI;
+    urlDB = process.env.a;
 };
 process.env.URLDB = urlDB;
